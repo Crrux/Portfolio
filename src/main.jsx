30c1283df2cfea10f17d5aa6d6bf89d8b6ee24b5
Mursch Arthur <arthur.mursch@gmail.com>
2024-05-28 23:56:46 +0200

    save

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import App from "./pages/App/App.jsx";
+
 import "./sass/index.css";
 
+import App from "./pages/App/App.jsx";
 import Home from "./pages/home/Home.jsx";
 import Error from "./pages/Error/Error.jsx";
 
@@ -11,10 +12,12 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <App />
+      {/* header */}
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="*" element={<Error />}></Route>
       </Routes>
+      {/* footer */}
     </Router>
   </React.StrictMode>
 );
