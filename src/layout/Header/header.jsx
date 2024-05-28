d42c0778e7fd67e9a6d3a740d48f00dd80bba229
Mursch Arthur <arthur.mursch@gmail.com>
2024-05-29 00:20:28 +0200

    Ajout Header / footer

---
+++ src/layout/Header/header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <main className="Header">
+      <h1>header</h1>
+    </main>
+  );
+}
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -4,20 +4,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./sass/index.css";
 
-import App from "./pages/App/App.jsx";
+import Header from "./layout/Header/header.jsx";
+import Footer from "./layout/Footer/Footer.jsx";
+
 import Home from "./pages/home/Home.jsx";
 import Error from "./pages/Error/Error.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <App />
-      {/* header */}
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="*" element={<Error />}></Route>
       </Routes>
-      {/* footer */}
+      <Footer />
     </Router>
   </React.StrictMode>
 );
