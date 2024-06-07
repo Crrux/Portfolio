edc576f0bc0c7b4b6d91458f9e34a3661438efe3
Mursch Arthur <arthur.mursch@gmail.com>
2024-06-07 18:44:27 +0200

    modif placement header + ajout nom

--- src/layout/Header/header.jsx
+++ src/layout/Header/header.jsx
@@ -5,11 +5,18 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <header className="Header">
-      <Link to={"/"}>Home</Link>
-      <Link to={"/contact"}>Contact</Link>
-      <a href="https://github.com/Crrux" target="_blank">
-        <img src={githubIcon} className="Header_GithubLogo" />
-      </a>
+      <p>MURSCH Arthur</p>
+      <nav className="Header_nav">
+        <Link to={"/"}>Home</Link>
+        <Link to={"/contact"}>Contact</Link>
+        <a
+          href="https://github.com/Crrux"
+          target="_blank"
+          className="Header_nav_link_GithubLogo"
+        >
+          <img src={githubIcon} className="Header_nav_GithubLogo" />
+        </a>
+      </nav>
     </header>
   );
 }
