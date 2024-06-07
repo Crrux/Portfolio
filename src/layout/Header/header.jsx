802b6d19ddb99ac16381d5ea02368c1005402ac6
Mursch Arthur <arthur.mursch@gmail.com>
2024-06-07 15:58:29 +0200

    save

--- src/layout/Header/header.jsx
+++ src/layout/Header/header.jsx
@@ -7,7 +7,7 @@ function Header() {
     <header className="Header">
       <Link to={"/"}>Home</Link>
       <Link to={"/contact"}>Contact</Link>
-      <a href="https://github.com/Crrux">
+      <a href="https://github.com/Crrux" target="_blank">
         <img src={githubIcon} className="Header_GithubLogo" />
       </a>
     </header>
