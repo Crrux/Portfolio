216b0dbb224db18ccde27c68633282c90d3d54a1
Mursch Arthur <arthur.mursch@gmail.com>
2024-06-08 16:43:53 +0200

    ajout logo

--- src/components/project_card/project_card.jsx
+++ src/components/project_card/project_card.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 function ProjectCard() {
   const [dataProjects, setData] = useState([]);
+  const [logos, setLogos] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -9,6 +10,7 @@ function ProjectCard() {
         const response = await fetch("/assets/database.json");
         const ProjectsData = await response.json();
         setData(ProjectsData);
+        setLogos(ProjectsData.logos);
       } catch (error) {
         console.error(error);
       }
@@ -39,7 +41,21 @@ function ProjectCard() {
                     />
                   </div>
                   <div className="Cards_Container_Info_Description">
-                    <p>test</p>
+                    <p>description</p>
+                    <div className="Cards_Container_Info_Description_imgcontainer">
+                      {project.used.map((logoName, index) => {
+                        const logo = logos.find((l) => l.title === logoName);
+                        return (
+                          <img
+                            key={index}
+                            className="logo"
+                            src={logo.URL}
+                            alt={logo.alt}
+                            title={logo.title}
+                          />
+                        );
+                      })}
+                    </div>
                   </div>
                 </div>
               </article>
