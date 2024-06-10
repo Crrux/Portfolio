31dc1e2d9a2635c277b1436b994af9f19ad3cf0d
Mursch Arthur <arthur.mursch@gmail.com>
2024-06-10 12:09:51 +0200

    save

--- src/components/logosvg/logosvg.jsx
+++ src/components/logosvg/logosvg.jsx
@@ -9,11 +9,7 @@ function Logosvg({ logos: elementsToCompare }) {
       try {
         const response = await fetch("/assets/database.json");
         const logosData = await response.json();
-
-        // Trier les éléments par "title" en ordre alphabétique
         logosData.logos.sort((a, b) => a.title.localeCompare(b.title));
-
-        // Effectuer un mappage avec tous les éléments du tableau dans logo
         const logoElements = logosData.logos.map((logo) => {
           if (elementsToCompare.includes(logo.title)) {
             return logo;
