d3a81f58df5c3cddc5d222266d66b85f7838b052
Mursch Arthur <arthur.mursch@gmail.com>
2024-06-24 16:17:30 +0200

    save

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,131 @@
-function Login() {
+import PropTypes from "prop-types";
+import { useState } from "react";
+import { storeInLocalStorage } from "../../lib/common";
+import { useNavigate } from "react-router";
+import axios from "axios";
+import { API_ROUTES } from "../../utils/constants";
+// import { APP_ROUTES } from "../../utils/constants";
+// import { useUser } from "../../lib/customHooks";
+
+function Login({ setUser }) {
+  const navigate = useNavigate();
+  //   const { user, authenticated } = useUser();
+  //     if (user || authenticated) {
+  //       navigate(APP_ROUTES.DASHBOARD);
+  //     }
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [notification, setNotification] = useState({
+    error: false,
+    message: "",
+  });
+  const signIn = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios({
+        method: "post",
+        url: API_ROUTES.SIGN_IN,
+        data: {
+          email,
+          password,
+        },
+      });
+      if (!response?.data?.token) {
+        setNotification({ error: true, message: "Une erreur est survenue" });
+        console.log("Something went wrong during signing in: ", response);
+      } else {
+        storeInLocalStorage(response.data.token, response.data.userId);
+        setUser(response.data);
+        navigate("/");
+      }
+    } catch (err) {
+      console.log(err);
+      setNotification({ error: true, message: err.message });
+      console.log("Some error occured during signing in: ", err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const signUp = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios({
+        method: "POST",
+        url: API_ROUTES.SIGN_UP,
+        data: {
+          email,
+          password,
+        },
+      });
+      if (!response?.data) {
+        console.log("Something went wrong during signing up: ", response);
+        return;
+      }
+      setNotification({
+        error: false,
+        message: "Votre compte a bien été créé, vous pouvez vous connecter",
+      });
+    } catch (err) {
+      setNotification({ error: true, message: err.message });
+      console.log("Some error occured during signing up: ", err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <main className="login">
-      <p>oui</p>
+      <div>
+        <div>
+          {notification.message.length > 0 && <p>{notification.message}</p>}
+        </div>
+        <div>
+          <label htmlFor={email}>
+            <p>Adresse email</p>
+            <input
+              className=""
+              type="text"
+              name="email"
+              id="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </label>
+          <label htmlFor="password">
+            <p>Mot de passe</p>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </label>
+          <div>
+            <button type="submit" onClick={signIn}>
+              {isLoading ? <div className="" /> : null}
+              <span>Se connecter</span>
+            </button>
+            <span>OU</span>
+            <button type="submit" onClick={signUp}>
+              {isLoading ? <div /> : null}
+              <span>{"S'inscrire"}</span>
+            </button>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
+
+Login.propTypes = {
+  setUser: PropTypes.func.isRequired,
+};
 
 export default Login;
