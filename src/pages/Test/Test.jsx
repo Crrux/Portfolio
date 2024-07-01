51a699154204bb6d2026235922bd4fe9e5c5c038
Mursch Arthur <arthur.mursch@gmail.com>
2024-07-01 15:00:19 +0200

    save

--- src/pages/Test/Test.jsx
+++ src/pages/Test/Test.jsx
@@ -1,4 +1,23 @@
+import React from "react";
+import { useState } from "react";
+import ReactDOM from "react-dom";
+import Modal from "react-modal";
+
+Modal.setAppElement("#root");
+
 function Test() {
-  return <div></div>;
+  const [modalIsOpen, setIsOpen] = useState(false);
+  function openModal() {
+    setIsOpen(true);
+  }
+  function closeModal() {
+    setIsOpen(false);
+  }
+  return (
+    <div>
+      <button onClick={openModal}>Open Modal</button>
+      <Modal isOpen={modalIsOpen} onRequestClose={closeModal}></Modal>
+    </div>
+  );
 }
 export default Test;
