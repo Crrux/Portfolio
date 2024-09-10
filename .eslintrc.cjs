568d8a1727960764f798a8cca47219de2839c665
Mursch Arthur <arthur.mursch@gmail.com>
2024-09-10 19:51:10 +0200

    save

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,8 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react/jsx-no-target-blank': 'off',
+    'react/no-unused-vars': 'error',
+    'react/no-undef': 'error',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
