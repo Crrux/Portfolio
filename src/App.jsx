4ab00e245a638ea2bb3431ed0b61440fd8a3069b
MURSCH Arthur <149969486+Crrux@users.noreply.github.com>
2025-02-13 20:04:59 +0100

    save

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ import Error from "./pages/Error/Error";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter future={{
+        v7_startTransition: true,
+      }}>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
