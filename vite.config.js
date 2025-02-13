dd98cce542d866648b83fe9b9119dd148641a98a
MURSCH Arthur <149969486+Crrux@users.noreply.github.com>
2025-02-13 18:59:04 +0100

    save

--- vite.config.js
+++ vite.config.js
@@ -3,23 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'robots.txt',
-          dest: ''
-        }
-      ]
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': '/src', // Example alias for easier imports
-    },
-  },
-  server: {
-    port: 3000
-  },
-});
+  plugins: [react()],
+})
