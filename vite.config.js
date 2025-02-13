261e6c0fce6da20157879750c084b07df8004a73
MURSCH Arthur <149969486+Crrux@users.noreply.github.com>
2025-02-13 17:19:02 +0100

    save

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,30 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'robots.txt',
+          dest: ''
+        }
+      ]
+    })
+  ],
+  server: {
+    host: '0.0.0.0',
+    port: 5174,
+    strictPort: true
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 5174,
+    strictPort: true,
+    allowedHosts: ['crossfitobernai.com', 'www.crossfitobernai.com']
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
+});
